89d51989036fd29ab515a2dcadfce8c024e9b12b
HonzikT <truxajan@gmail.com>
2020-03-31 14:23:49 +0200

    Updates

---
+++ precache-manifest.dc818ae554237c67f4ce69ec73b603e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84b36a906c9961ef8b88f1edc725e665",
+    "url": "/react-recipes/index.html"
+  },
+  {
+    "revision": "683fa4ca6cfe5bd14b2f",
+    "url": "/react-recipes/static/css/main.1036591a.chunk.css"
+  },
+  {
+    "revision": "288e596576eb89b6a22a",
+    "url": "/react-recipes/static/js/2.aafc4890.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-recipes/static/js/2.aafc4890.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "683fa4ca6cfe5bd14b2f",
+    "url": "/react-recipes/static/js/main.3dbc402d.chunk.js"
+  },
+  {
+    "revision": "9945f01f8bf311a0fbd2",
+    "url": "/react-recipes/static/js/runtime-main.2121fbb3.js"
+  }
+]);
